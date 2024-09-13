00c6ade5001778f7b1bb33da3b4ebfa1ab6dcd01
mandeepsingh <mandeepsinghsaggu2@gmail.com>
2024-09-14 00:36:45 +0530

    commit 2

--- app/src/main/java/com/example/recyclerviewconsitencyapp/MyAdapter.kt
+++ app/src/main/java/com/example/recyclerviewconsitencyapp/MyAdapter.kt
@@ -14,14 +14,22 @@ class MyAdapter : ListAdapter<Item, MyAdapter.MyViewHolder>(DiffUtils) {
     inner class MyViewHolder(val binding : ItemLayoutBinding) : ViewHolder(binding.root){
 
         var textWatcher1 : TextWatcher? = null
+        var textWatcher2 : TextWatcher? = null
+        var textWatcher3 : TextWatcher? = null
 
         fun bind(item : Item){
 
 
             textWatcher1?.let { binding.editText.removeTextChangedListener(textWatcher1) }
+            textWatcher2?.let { binding.editText2.removeTextChangedListener(textWatcher2) }
+            textWatcher3?.let { binding.editText3.removeTextChangedListener(textWatcher3) }
 
             textWatcher1 = null
+            textWatcher2 = null
+            textWatcher3 = null
             binding.editText.setText(item.text)
+            binding.editText2.setText(item.text2)
+            binding.editText3.setText(item.text3)
             textWatcher1 = object : TextWatcher{
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 }
@@ -34,7 +42,33 @@ class MyAdapter : ListAdapter<Item, MyAdapter.MyViewHolder>(DiffUtils) {
 
                 }
             }
+            textWatcher2 = object : TextWatcher{
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun afterTextChanged(text: Editable?) {
+                    item.text2 = text.toString()
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+            }
+            textWatcher3 = object : TextWatcher{
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun afterTextChanged(text: Editable?) {
+                    item.text3 = text.toString()
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+            }
             binding.editText.addTextChangedListener(textWatcher1)
+            binding.editText2.addTextChangedListener(textWatcher2)
+            binding.editText3.addTextChangedListener(textWatcher3)
 
 
 
